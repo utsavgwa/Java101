2718c627e994436e9a6c84ec70b49f82be1fa810
Utsav S <utsavgwa@gmail.com>
2023-03-16 02:40:02 +0530

    java decision operators

---
+++ src/main/java/decision_structures/SalaryCalculator.java
@@ -0,0 +1,9 @@
+package decision_structures;
+
+public class SalaryCalculator {
+    public static void main(String[] args) {
+        double salary = 1000;
+        double bonus = 250;
+        int quota = 10;
+    }
+}

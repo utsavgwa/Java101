8e376fe44e4b9585396fb4549e5836991c680af6
Utsav S <utsavgwa@gmail.com>
2023-03-16 11:11:18 +0530

    eg. if statement

--- src/main/java/decision_structures/SalaryCalculator.java
+++ src/main/java/decision_structures/SalaryCalculator.java
@@ -1,9 +1,20 @@
 package decision_structures;
 
+import java.util.Scanner;
+
 public class SalaryCalculator {
     public static void main(String[] args) {
         double salary = 1000;
         double bonus = 250;
         int quota = 10;
+
+        System.out.println("How many sales did the employee get this week?");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+
+        if (sales > quota){
+            salary = salary+bonus;
+        }
+        System.out.println("Salary: " + salary);
     }
 }

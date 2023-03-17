b7cce5fff9226136008edcf0af883611cd17075d
Utsav S <utsavgwa@gmail.com>
2023-03-18 03:44:33 +0530

    feat, overloaded methods, different signature

---
+++ src/main/java/methods/NewMethodOver.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class NewMethodOver {
+    // takes a month name, gives month number in 12
+    public int getMonth(String month){
+
+    }
+    // takes a month number, gives month name
+    public String getMonth(int month){
+
+    }
+}
